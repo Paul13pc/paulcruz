f15ffb8f32e143a369712cedd9a11536b6a5dcdc
paul13pc <yandripaul13@gmail.com>
2023-10-15 21:40:46 -0500

    first commit

---
+++ portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .models import Project
+
+# Create your views here.​
+
+def home(request):
+    projects = Project.objects.all()
+
+    return render(request,'home.html', {'projects': projects})
